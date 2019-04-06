17df923c2da96337cb98115dfd5d331f65f580ee
brian becker <brianbecker@brians-Air.domain>
2019-04-06 14:07:40 -0600

    shopping pages

---
+++ src/pages/home/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./styles.css";
+import "./App.css";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>I am in the header</header>
+        <main>i am in the main</main>
+        <footer>i am in the footer</footer>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ src/pages/landing/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import MyHeader from "../../components/header";
+import "./styles.css";
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div className="Landing-page">
+        <div className="main">1</div>
+        <div id="main-left" /></div>
+        <div id='main-right' /></div>
+
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
